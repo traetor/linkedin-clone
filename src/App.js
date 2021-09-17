d047dbe051523019dc3b56df30cbb94b43b9f586
JKUltimateSystems <j.krol@ultimate.systems>
2021-09-17 22:21:11 +0200

    test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        <Counter />test
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
